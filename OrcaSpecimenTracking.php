e98707759dd6186a9b1efbd1b6b332dcab35ca3f
dr01d3r <droider007@gmail.com>
2023-08-22 10:26:16 -0500

    removed unnecessary constructor

--- OrcaSpecimenTracking.php
+++ OrcaSpecimenTracking.php
@@ -86,11 +86,6 @@ class OrcaSpecimenTracking extends AbstractExternalModule {
         ]
     ];
 
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
     public function redcap_module_link_check_display($project_id, $link) {
         return true;
     }
